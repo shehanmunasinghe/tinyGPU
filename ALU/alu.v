cfd452ba5316452ad4499740a7582e59e5502edc
shehanmunasinghe <shehanmunasinghe@gmail.com>
2020-10-07 22:29:28 +0530

    enforce syntax conventions

--- ALU/alu.v
+++ ALU/alu.v
@@ -3,42 +3,42 @@ module alu(
     input [15:0] B,  	
     input [15:0] C, 							// ALU 16-bit Inputs                 
     input [3:0] ALU_c,							// ALU control 4-bit
-    output [15:0] ALU_Out, 					// ALU 16-bit Output
-    output z_Out 								// Zero Flag
+    output [15:0] ALU_out, 					// ALU 16-bit Output
+    output Z_out 								// Zero Flag
     );
 	 
 	 
-    reg [15:0] ALU_Result;
+    reg [15:0] ALU_result;
     reg z = 1'b0 ;
     
-    assign ALU_Out = ALU_Result; 			// ALU out
-    assign z_Out = z ;
+    assign ALU_out = ALU_result; 			// ALU out
+    assign Z_out = z ;
     
     always @(*) begin			
         case(ALU_c)
             4'b0001: 									// CLEAR x
-                ALU_Result <= 1'b0 ; 
+                ALU_result <= 1'b0 ; 
 			  			  
             4'b0010: 									// INC x
-                ALU_Result <= A + 1'b1 ;
+                ALU_result <= A + 1'b1 ;
                 
             4'b0111: 									// ADD x y z
-                ALU_Result <= A + B ;
+                ALU_result <= A + B ;
                 
             4'b1000: 									// MUL x y z
-                ALU_Result <= A * B ;
+                ALU_result <= A * B ;
                 
             4'b1001: 									// MAD x y z
-                ALU_Result <= A + (B * C) ;
+                ALU_result <= A + (B * C) ;
                 
             4'b1010: 									// AND x y 
-                ALU_Result <= A & B ;
+                ALU_result <= A & B ;
                 
             4'b1011: 									// CMPL x y 
                 if (A < B) 
-                    ALU_Result <= 1'b1 ;
+                    ALU_result <= 1'b1 ;
                 else 
-                    ALU_Result <= 1'b0 ;
+                    ALU_result <= 1'b0 ;
                         
             4'b1100: 									// BEQ x y 
                 if (A == B) 
@@ -53,7 +53,7 @@ module alu(
                     z <= 1'b1 ;
                         					
             default: 
-                ALU_Result = A + B ; 
+                ALU_result = A + B ; 
 
         endcase
     end

a454450905a162674aab23c3cdcd71c1c88bb479
shehanmunasinghe <shehanmunasinghe@gmail.com>
2021-07-01 20:52:22 +0530

    Combine entire processor into a single Verilog module

---
+++ Verilog/Modules/System.v
@@ -0,0 +1,24 @@
+module System (
+    input clk,
+    input reset );
+    
+    wire memclk;
+    // assign memclk=~clk;
+    assign memclk=clk;
+
+    //Connections to Instruction Memory
+    wire  [`INSTMEM_ADDR_WIDTH-1:0]   inst_addr;
+    wire   [`INST_LENGTH-1:0]          inst;
+
+    //Connections to Data Memory
+    wire  [`DATAMEM_ADDR_WIDTH-1:0]   DataAddress;
+    wire  [`DATA_WORD_LENGTH-1:0]     DataToWrite;
+    wire   [`DATA_WORD_LENGTH-1:0]     DataToRead;
+    wire  DataMemWrEn;
+
+    // Modules
+    DataMemory DMem(memclk,DataMemWrEn,DataAddress,DataToWrite,DataToRead);
+    InstructionMemory IMem(clk,inst_addr,inst);
+    SMCore SMCore(inst_addr,inst,DataAddress,DataToWrite,DataToRead,DataMemWrEn, clk, reset);
+
+endmodule
